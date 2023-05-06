b3962a2af7e0f61a899a68d7b738eb70d76ce885
priyanshu <priyanshukumar644@gail.com>
2023-05-06 20:24:41 +0530

    Branch a first commit

---
+++ ass14.java
@@ -0,0 +1,10 @@
+class ass14
+{
+ public static void main(String args[])
+ {
+  String str="Haldia Institute of Technology";
+  int l=str.length();
+  String s=str.substring(l-10,l);
+  System.out.println("The required substring is \n"+s);
+ }
+}
