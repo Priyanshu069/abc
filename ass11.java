daf46fd1b8518b6b3bc887754f6c27d474e9414d
priyanshu <priyanshukumar644@gail.com>
2023-05-06 19:02:31 +0530

    ass11 commited

---
+++ ass11.java
@@ -0,0 +1,13 @@
+class ass11
+{
+ public static void main(String args[])
+ {
+  double a[]={3.4,3.5,3.6,3.7};
+  double sum=0;
+  for(int i=0; i<4; i++)
+  {
+   sum=sum+a[i];
+  }
+  System.out.println(sum/4.0);
+ }
+}
