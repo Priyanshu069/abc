805846ae1bd4449d3756c8f6ac527c011821c311
Priyanshu <priyanshukumar644@gmail.com>
2023-05-08 23:41:30 +0530

    ass15.java added

---
+++ ass15.java
@@ -0,0 +1,9 @@
+class ass15
+{
+ public static void main(String args[])
+ {
+  char ch[]={'1','2','3','4','5'};
+  String str=new String(ch);
+  System.out.println("The last number is : "+str.charAt(4));
+ }
+}
